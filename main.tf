686384ecfe785a816df4b9c23a8a1a065a30b911
Jason Scalia <jscalia@pm.me>
2022-04-10 22:38:34 -0500

    add identity

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     origin_id   = local.s3_origin_id
 
     s3_origin_config {
-      origin_access_identity = "origin-access-identity/cloudfront/ABCDEFG1234567"
+      origin_access_identity = "access-identity-jasonscalia.com.s3.us-east-1.amazonaws.com"
     }
   }
 
